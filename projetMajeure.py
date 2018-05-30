a52b027cefa68c3057336524afa19c59c656c1d3
ERBACHER Pierre <bot_@hotmail.fr>
2018-05-30 17:51:02 +0200

    Update projetMajeure.py

--- projetMajeure.py
+++ projetMajeure.py
@@ -11,6 +11,10 @@ from PyQt5.QtWidgets import QApplication, QWidget
 PV_INITIAL = 3
 DAMAGE = 1
 RESOURCE_VALUE = 50
+#team = 0 => ressource et block
+#team = 1 => agent et tir (équipe bleu)
+#team = 12 => agent et tir (équipe rouge)
+
 
 class Objet:
 # classe pour tout les object qui sont dans l'environnement
@@ -20,12 +24,13 @@ class Objet:
 	dy = 0
 	box_x = 5 #collision boxes ( largeur et longeur de l'objet)
 	box_y = 5
-	def __init__(self,x=0,y=0, dx=0, dy=0):
+	team = 0
+	def __init__(self,x=0,y=0, dx=0, dy=0 ,team = 0):
 		self.x = x;
 		self.y = y;
 		self.dx = dx;
 		self.dy = dy;
-
+		self.team = team;
 	def move(self):
 	# bouger l'agent d'accord avec sa vitesse
 		self.x = self.x + self.dx;
@@ -40,12 +45,16 @@ class Objet:
 			ret = True
 		return ret
 			
+	def draw(self):
+		#affichage de l'objet
+		pass #TODO
 		
+
 		
 class Agent(Objet):
 # classe pour les agents
-	def __init__(self,x=0,y=0, dx=0, dy=0):
-		Objet.__init__(self,x,y,dx,dy);
+	def __init__(self,x=0,y=0, dx=0, dy=0,team = 0):
+		Objet.__init__(self,x,y,dx,dy,team);
 		self.pv = PV_INITIAL
 
 	def shoot(self):
@@ -62,7 +71,7 @@ class Agent(Objet):
 class Tir:
 # classe pour les tirs
 	def __init__(self,x=0,y=0, dx=0, dy=0):
-		Objet.__init__(self,x,y,dx,dy);
+		Objet.__init__(self,x,y,dx,dy,team);
 		self.dmg = DAMAGE
 		
 class Block:
