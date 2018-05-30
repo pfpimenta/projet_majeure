fe881cd9c29104815ebeb1ca79c7a3fc3ebeb1d2
ERBACHER Pierre <bot_@hotmail.fr>
2018-05-30 17:38:11 +0200

    Update projetMajeure.py

--- projetMajeure.py
+++ projetMajeure.py
@@ -9,6 +9,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 PV_INITIAL = 3
+DAMAGE = 1
+RESOURCE_VALUE = 50
 
 class Objet:
 # classe pour tout les object qui sont dans l'environnement
@@ -16,6 +18,8 @@ class Objet:
 	y = 0
 	dx = 0 # vitesses
 	dy = 0
+	box_x = 5 #collision boxes ( largeur et longeur de l'objet)
+	box_y = 5
 	def __init__(self,x=0,y=0, dx=0, dy=0):
 		self.x = x;
 		self.y = y;
@@ -26,7 +30,18 @@ class Objet:
 	# bouger l'agent d'accord avec sa vitesse
 		self.x = self.x + self.dx;
 		self.y = self.y + self.dy;
-
+	
+	def collision(self,obj ):# how to pass object ?
+	#verifie la collision entre les objets
+		ret = False
+		if (self.x + self.box_x < obj.x - obj.box_x && self.x - self.box_x > obj.x + obj.box_x && self.y + self.box_y < obj.y - obj.box_y && self.y - self.box_y > obj.y + obj.box_y):
+    			ret = False
+		else:
+			ret = True
+		return ret
+			
+		
+		
 class Agent(Objet):
 # classe pour les agents
 	def __init__(self,x=0,y=0, dx=0, dy=0):
@@ -44,7 +59,23 @@ class Agent(Objet):
 	# placer un bloque en face du agent
 		pass # TODO
 	
-
+class Tir:
+# classe pour les tirs
+	def __init__(self,x=0,y=0, dx=0, dy=0):
+		Objet.__init__(self,x,y,dx,dy);
+		self.dmg = DAMAGE
+		
+class Block:
+# classe pour les blocks Mur
+	def __init__(self,x=0,y=0, dx=0, dy=0):
+		Objet.__init__(self,x,y);
+		
+class Resource:
+# classe pour les rescources
+	def __init__(self,x=0,y=0, dx=0, dy=0):
+		Objet.__init__(self,x,y);
+		self.value =  RESOURCE_VALUE
+		
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
